844d99dccdeae1b25272a2782f76ff3114584807
Shaxrizod071 <ahtamkulovshaxrizod@gmail.com>
2025-04-28 13:54:24 +0500

    salom2

--- arithmetic_op01.py
+++ arithmetic_op01.py
@@ -1,5 +1,7 @@
 # Create a variable 'num_one'  and assign it the value of the int 5.
-
+a=5
 # Create a variable 'num_two'   and assign it the value of the int 4.
-
+b=4
 # Create a variable 'variable_total' and assign it the sum of  'num_one' and 'num_two'.
+c=a+b
+print(c)
